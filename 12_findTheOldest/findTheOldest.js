ed21555d596bc55a7acc8ea7cd90e0c1748e48b8
Justy Oberg <justyoberg1@gmail.com>
2022-09-20 09:37:10 -0700

    Complete Fundamentals part 5

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+  let highest = 0;
+  let returnObj = {};
+  for (let obj of arr){
+    if (!obj.yearOfDeath) {
+      obj.yearOfDeath = new Date().getFullYear();
+    }
+    if ((obj.yearOfDeath - obj.yearOfBirth) > highest) {
+      highest = (obj.yearOfDeath - obj.yearOfBirth);
+      returnObj = obj;
+    }
+  }
+  return returnObj;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
